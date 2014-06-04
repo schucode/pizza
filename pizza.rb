4c5c01874dbf8cc4d2616cb5a46acb5c2ec847db
vagrant <vagrant@student-dev-box>
2014-06-04 11:32:22 -0500

    Add successful code

--- pizza.rb
+++ pizza.rb
@@ -1,8 +1,37 @@
 class Pizza
 
-	def initialize(toppings)
-		
+	attr_accessor :toppings, :delivery_time
+
+	def initialize(toppings = [])
+		@toppings = toppings << 'cheese'
 	end
+
+	def vegetarian?()
+		if @toppings.find {|i| i.vegetarian} != 0
+			return true
+		else
+			return false
+		end
+	end
+
+	def add_topping(topping) 
+		@toppings << topping
+	end
+
+	def deliver!
+		@delivery_time = Time.now + (60*30)
+		return @delivery_time
+	end
+
+	def late?
+		t = Time.now
+		if @delivery_time > t
+			return false
+		else
+			return true
+		end
+	end
+
 end
 
 class Topping
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -11,9 +11,52 @@ describe Pizza do
         Topping.new('mushrooms', vegetarian: true),
         Topping.new('pepperoni')
       ]
+
       pizza = Pizza.new(toppings)
 
       expect(pizza.toppings).to eq(toppings) 
+    end
+  end
+
+  describe 'vegetarian?' do
+    it 'tells you whether the pizza is vegetarian' do
+      toppings = [ Topping.new('mushrooms', vegetarian: true)]
+
+      pizza = Pizza.new(toppings)
+
+      expect(pizza.vegetarian?).to eq(true)
+    end
+  end
+
+  describe 'add_topping' do
+    it 'adds a topping to the pizza' do
+      pizza = Pizza.new()
+
+      expect(pizza.toppings).to eq(['cheese'])
+
+      topping = Topping.new('peppers')
+
+      pizza.add_topping(topping)
+
+      expect(pizza.toppings).to eq(['cheese', topping])
+    end
+  end
+
+  describe 'deliver!' do
+    it 'sets a delivery time 30min from now' do
+      pizza = Pizza.new
+      pizza.deliver!
+      now = (Time.now + (60*30)).min
+      expect(pizza.delivery_time.min).to eq(now)
+    end
+  end
+
+  describe 'late?' do
+    it 'tells you if the delivery is running late' do
+      pizza = Pizza.new()
+      pizza.deliver!
+
+      expect(pizza.late?).to eq(false)
     end
   end
 
